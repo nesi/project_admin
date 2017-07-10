783029ccba168d4064325a0fc1d6a72f17b07bc1
kumraja <kumaresh.rajalingam@nesi.org.nz>
2017-07-10 15:43:36 +1200

    Change app property path

--- src/main/java/nz/org/nesi/collaboration/AppConfig.java
+++ src/main/java/nz/org/nesi/collaboration/AppConfig.java
@@ -4,9 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+//@PropertySource("file:E:\\etc\\application.properties")
 
 @Configuration
-@PropertySource("file:E:\\etc\\application.properties")
+@PropertySource("file:/etc/application.properties")
 public class AppConfig {
 	@Autowired
 	Environment env;
